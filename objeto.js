f693a8c1ef72e590e0f3fe53c5988687b1dd8e32
Caitano <josiasmartinscaitano@gmail.com>
2022-03-31 17:56:40 -0300

    feature(print-infos.js) adiconado o objeto javascript

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = ["12343444545", "23244545", "343434344"];
+
+// const cliente = ["nome", "André", "idade", 36];
+
+const clientes = {
+    nome: "André",
+    idade: 36,
+    cpf: "1222344545",
+    email: "andre@email.com"
+};
+
